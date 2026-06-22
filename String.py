7b67a6524ca5e072ff99b978a7bb2a25cf1767ed
PedroVeicir <pedrohenriqueveicir@gmail.com>
2026-06-22 14:24:35 -0300

    Refactor String.py by cleaning up code
    Removed comments and unnecessary lines from String.py.

--- String.py
+++ String.py
@@ -1,26 +1,19 @@
-# questão 01
 nome = input("Digite seu primeiro nome: ")
 sobrenome = input("Digite seu sobrenome: ")
 
 nome_completo = nome + " " + sobrenome
 print("Bem-vinda, " + nome_completo + "!")
 
-
-# questão 02
 frase = input("Digite a frase: ")
 
 espacos = frase.count(" ")
 print("Espaços em branco:", espacos)
 
-
-# questão 03
 nome = input("Digite seu nome: ")
 
 for i in range(len(nome)):
     print(nome[:i + 1])
 
-
-# questão 04
 numero = input("Digite o número: ")
 
 if len(numero) == 8:
@@ -30,8 +23,6 @@ elif len(numero) == 9 and numero[0] != "9":
 
 print("Número completo:", numero[:5] + "-" + numero[5:])
 
-
-# questão 05
 frase = input("Digite uma frase: ")
 
 indices = []
